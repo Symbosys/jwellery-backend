ef61b0bb5d3f77dc92ce9e6adc3ab02805321577
Developer <developer@example.com>
2026-07-17 12:12:51 +0530

    fixed the error

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,8 @@ import addressrouter from "./module/address/routes/address.routes.js";
 const app = express();
 
 const allowedOrigins = [
-  "https://protien-frontend.vercel.app",
-  "https://protien-admin.vercel.app",
+  "https://jwellery-frontend-seven.vercel.app",
+  "https://jwellery-admin-delta.vercel.app",
 ];
 
 app.use(
@@ -33,7 +33,7 @@ app.use(
     origin: (origin, callback) => {
       // Allow requests with no origin (like mobile apps, curl, postman)
       if (!origin) return callback(null, true);
-      
+
       if (
         allowedOrigins.includes(origin) ||
         /^https?:\/\/(localhost|127\.0\.0\.1|192\.168\.\d+\.\d+|10\.\d+\.\d+\.\d+)(:\d+)?$/.test(origin)
